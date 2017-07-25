1c8e907321f958f752557ca33e15c9cc589469da
Dan Aschenbach <phily_phreak@yahoo.com>
2017-07-25 10:34:54 -0500

    Add files via upload

---
+++ tournament/tournament.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# 
+# tournament.py -- implementation of a Swiss-system tournament
+#
+
+import psycopg2
+
+
+def connect():
+    """Connect to the PostgreSQL database.  Returns a database connection."""
+    return psycopg2.connect("dbname=tournament")
+
+
+def deleteMatches():
+    """Remove all the match records from the database."""
+    db = connect()
+    c = db.cursor()
+    c.execute("DELETE FROM Matches")
+    db.commit()
+    db.close()
+
+def deletePlayers():
+    """Remove all the player records from the database."""
+    db = connect()
+    c = db.cursor()
+    c.execute("DELETE FROM Players")
+    db.commit()
+    db.close()
+
+def countPlayers():
+    """Returns the number of players currently registered."""
+    db = connect()
+    c = db.cursor()
+    c.execute("SELECT count(*) FROM Players")
+    player_ct = c.fetchone()[0]
+    db.commit()
+    db.close()
+    return player_ct
+
+def registerPlayer(name):
+    """Adds a player to the tournament database.
+  
+    The database assigns a unique serial id number for the player.  (This
+    should be handled by your SQL database schema, not in your Python code.)
+  
+    Args:
+      name: the player's full name (need not be unique).
+    """
+    db = connect()
+    c = db.cursor()
+    c.execute("INSERT INTO Players(fullname) values(%s)", (name,))
+    db.commit()
+    db.close()
+
+def playerStandings():
+    """Returns a list of the players and their win records, sorted by wins.
+
+    The first entry in the list should be the player in first place, or a player
+    tied for first place if there is currently a tie.
+
+    Returns:
+      A list of tuples, each of which contains (id, name, wins, matches):
+        id: the player's unique id (assigned by the database)
+        name: the player's full name (as registered)
+        wins: the number of matches the player has won
+        matches: the number of matches the player has played
+    """
+    db = connect()
+    c = db.cursor()
+    c.execute("SELECT * FROM number_of_wins ORDER by wins")
+    player_st = c.fetchall()
+    return player_st
+    db.commit()
+    db.close()
+
+def reportMatch(winner, loser):
+    """Records the outcome of a single match between two players.
+
+    Args:
+      winner:  the id number of the player who won
+      loser:  the id number of the player who lost
+    """
+    db = connect()
+    c = db.cursor()
+    c.execute("INSERT INTO Matches(winner, loser) values(%s, %s)", (winner, loser,))
+    db.commit()
+    db.close()
+ 
+def swissPairings():
+    """Returns a list of pairs of players for the next round of a match.
+  
+    Assuming that there are an even number of players registered, each player
+    appears exactly once in the pairings.  Each player is paired with another
+    player with an equal or nearly-equal win record, that is, a player adjacent
+    to him or her in the standings.
+  
+    Returns:
+      A list of tuples, each of which contains (id1, name1, id2, name2)
+        id1: the first player's unique id
+        name1: the first player's name
+        id2: the second player's unique id
+        name2: the second player's name
+    """
+    db = connect()
+    c = db.cursor()
+    temp = []
+    c.execute("SELECT id, fullname FROM number_of_wins Order by wins DESC")
+    value = c.fetchall()
+    for i in range(1, len(value), 2):
+        one = value[i - 1]
+        two = value[i]
+        temp.append((one[0], one[1], two[0], two[1]))
+    for pairs in temp:
+        return temp
+    db.commit()
+    db.close()
+    
+
---
+++ tournament/tournament.sql
@@ -0,0 +1,22 @@
+-- Table definitions for the tournament project.
+--
+-- Put your SQL 'create table' statements in this file; also 'create view'
+-- statements if you choose to use it.
+--
+-- You can write comments in this file by starting them with two dashes, like
+-- these lines here.
+CREATE TABLE Players (ID serial primary key, fullname text);
+
+CREATE TABLE Matches (match_ID serial primary key,
+	winner integer references Players(ID),
+	loser integer references Players(ID));
+
+CREATE VIEW number_of_wins AS SELECT Players.id, Players.fullname,
+	(SELECT Count(Matches.winner)
+	FROM Matches
+	WHERE Matches.winner = Players.id) AS wins,
+	(SELECT Count(Matches.match_ID)
+	FROM Matches
+	WHERE Matches.loser = Players.id OR Matches.winner = Players.id)
+	AS Matches FROM Players;
+
