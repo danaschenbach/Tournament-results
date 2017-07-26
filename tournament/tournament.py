f0c38ab1b4f96365d71bd65a63347ade59447737
Dan Aschenbach <phily_phreak@yahoo.com>
2017-07-26 12:14:15 -0500

    remove white space/notes

--- tournament/tournament.py
+++ tournament/tournament.py
@@ -39,12 +39,7 @@ def countPlayers():
     db.close()
 
 def registerPlayer(name):
-    """Adds a player to the tournament database.
-    The database assigns a unique serial id number for the player.  (This
-    should be handled by your SQL database schema, not in your Python code.)  
-    Args:
-      name: the player's full name (need not be unique).
-    """
+    """Adds a player to the tournament database"""
     """suggested by reviewer"""
     db, c = connect()
     query = 'INSERT INTO players(name) VALUES(%s);'
@@ -56,14 +51,7 @@ def registerPlayer(name):
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
     The first entry in the list should be the player in first place, or a player
-    tied for first place if there is currently a tie.
-    Returns:
-      A list of tuples, each of which contains (id, name, wins, matches):
-        id: the player's unique id (assigned by the database)
-        name: the player's full name (as registered)
-        wins: the number of matches the player has won
-        matches: the number of matches the player has played
-    """
+    tied for first place if there is currently a tie."""
     db = connect()
     c = db.cursor()
     query = 'SELECT * FROM number_of_wins ORDER by wins;'
@@ -72,11 +60,7 @@ def playerStandings():
     db.close()
 
 def reportMatch(winner, loser):
-    """Records the outcome of a single match between two players.
-    Args:
-      winner:  the id number of the player who won
-      loser:  the id number of the player who lost
-    """
+    """Records the outcome of a single match between two players."""
     db = connect()
     c = db.cursor()
     query = "INSERT INTO Matches(winner, loser) VALUES(%s, %s);"
@@ -90,14 +74,7 @@ def swissPairings():
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
-    to him or her in the standings.
-    Returns:
-      A list of tuples, each of which contains (id1, name1, id2, name2)
-        id1: the first player's unique id
-        name1: the first player's name
-        id2: the second player's unique id
-        name2: the second player's name
-    """
+    to him or her in the standings."""
     db = connect()
     c = db.cursor()
     temp = []
